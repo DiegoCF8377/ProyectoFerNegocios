6a04aebdbb9efafb8ff21237bd37273cd3423bc5
CannedTomato <96097813+CannedTomato@users.noreply.github.com>
2023-10-06 23:23:35 -0500

    XD

---
+++ Main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.write("""SExo""")
+st.image("ImagenEnTiktok\SusDoggo.jpg", caption="Descripción de la imagen", width=300)
+st.image("Productos\Payaso.png", caption="Descripción de la imagen", width=300)
