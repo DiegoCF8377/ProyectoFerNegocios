5afa7814332805bcbda0b361ce4fe2dd04fa478a
CannedTomato <96097813+CannedTomato@users.noreply.github.com>
2023-10-07 00:22:17 -0500

    XDesdePython

--- Main.py
+++ Main.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 
 st.write("""SExo""")
-st.image("SusDoggo.jpg", caption="Latest Popular Tiktok", width=300)
+st.image("captura.png", caption="Latest Popular Tiktok", width=300)
 st.image("Payaso.png", caption="Zapatos finos :D", width=300)
