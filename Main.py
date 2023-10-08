68018521520e911eda25fb72e1181c635c662e9d
CannedTomato <96097813+CannedTomato@users.noreply.github.com>
2023-10-08 13:47:29 -0500

    song

--- Main.py
+++ Main.py
@@ -9,3 +9,12 @@ st.write("Aquí encontrarás Lo ultimo en moda")
 # Imágenes con títulos y dimensiones personalizadas
 st.image("captura.png", caption="Últimos videos populares en TikTok", width=300)
 st.image("Payaso.png", caption="Zapatos elegantes :D", width=300)
+st.markdown(
+    """
+    <audio autoplay controls>
+      <source src="Balenciaga meme song  Thip Trong  Lightvessel.mp3" type="audio/mpeg">
+      Tu navegador no soporta la reproducción de audio.
+    </audio>
+    """,
+    unsafe_allow_html=True,
+)
