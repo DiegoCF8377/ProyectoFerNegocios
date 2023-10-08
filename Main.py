0d28ac6c7101a6beb50a075f52baeb19d6cc4e00
CannedTomato <96097813+CannedTomato@users.noreply.github.com>
2023-10-08 14:14:55 -0500

    video

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ st.image("Principa.png",use_column_width=True)
 
 # Título con estilo
 st.title("Pisa fuerte, marca tendencia con Capa de Ozono")
-
+st.video("video.mp4", format="video/mp4", start_time=0)
 # Texto con formato
 st.subheader("""Aquí encontrarás Lo ultimo en moda""")
 
