d7f8672d56efad27a7d50c12e365a5c8595e4ecf
CannedTomato <96097813+CannedTomato@users.noreply.github.com>
2023-10-08 13:40:11 -0500

    df

--- Main.py
+++ Main.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
+# Título con estilo
+st.title("¡Bienvenido a mi aplicación de Streamlit!")
 
-st.write("""SExo""")
-st.image("captura.png", caption="Latest Popular Tiktok", width=300)
-st.image("Payaso.png", caption="Zapatos finos :D", width=300)
+# Texto con formato
+st.write("Aquí encontrarás contenido interesante sobre diferentes temas.")
+
+# Imágenes con títulos y dimensiones personalizadas
+st.image("captura.png", caption="Últimos videos populares en TikTok", use_column_width=True)
+st.image("Payaso.png", caption="Zapatos elegantes :D", use_column_width=True)
